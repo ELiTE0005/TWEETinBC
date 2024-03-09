d1ca94a5623a640b0dd664f3391755a892989b32
RAJBIR BISWAS <146480609+ELiTE0005@users.noreply.github.com>
2024-03-09 18:31:06 +0530

    Add files via upload
    Add a tweet to a BLOCKCHAIN  and get the tweet using the WALLET ADDRESS 
    
    can also perform in any TESTNET ( Sepolia preffered )

---
+++ Twitter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Twitter {
+
+    mapping(address => string) public tweets;
+
+    function createTweet(string memory _tweet) public {
+
+        tweets[msg.sender] = _tweet;
+    }
+
+
+    function getTweet(address _owner) public view returns (string memory) {
+
+        return tweets[_owner];
+    }
+}
