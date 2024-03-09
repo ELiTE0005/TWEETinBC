10571dcdf414aae26bbd3d2467ba8062103a0c0d
RAJBIR BISWAS <146480609+ELiTE0005@users.noreply.github.com>
2024-03-09 18:56:37 +0530

    Add files via upload
    arrays of all tweets  stores in a single wallet address

---
+++ Twitter (1).sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Twitter {
+
+    mapping(address => string[]) public tweets;
+
+    function createTweet(string memory _tweet) public {
+
+        tweets[msg.sender].push(_tweet);
+    }
+
+
+    function getTweet(address _owner , uint _i) public view returns (string memory) {
+
+        return tweets[_owner][_i];
+    }
+
+
+    function getAllTweets(address _owner) public view returns (string[] memory ) {
+        return tweets[_owner];
+    }
+}
